6320205c0da3d0e0708e541473f9934c3b3fbde1
MohammedFarhanR <67556309+MohammedFarhanR@users.noreply.github.com>
2020-06-28 23:41:37 +0530

    Update LinearSearch.java

--- LinearSearch.java
+++ LinearSearch.java
@@ -2,11 +2,12 @@ public class LinearSearch
 {
     public int search(int arr[], int searchElement)
     {
+        //We traverse through the entire array 
         for(int index = 0; index < arr.length; index ++)
         {
-            if(arr[i] == searchElement)
+            if(arr[i] == searchElement) //Checks if the element at position i is the same as the searchElement
                 return index;
         }
-        return -1;
+        return -1; 
     }
 }
