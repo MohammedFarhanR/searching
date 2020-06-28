13c56e51864651d6b823343162a36e924d4cc4c6
MohammedFarhanR <67556309+MohammedFarhanR@users.noreply.github.com>
2020-06-28 23:40:24 +0530

    Create LinearSearch.java

---
+++ LinearSearch.java
@@ -0,0 +1,12 @@
+public class LinearSearch
+{
+    public int search(int arr[], int searchElement)
+    {
+        for(int index = 0; index < arr.length; index ++)
+        {
+            if(arr[i] == searchElement)
+                return index;
+        }
+        return -1;
+    }
+}
